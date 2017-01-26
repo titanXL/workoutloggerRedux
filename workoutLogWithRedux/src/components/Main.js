9e415c299cb5ee112d4d9b3e53b3e86aa20c7f62
titanXL <emildavidkov88@gmail.com>
2017-01-26 11:26:21 +0200

    'passingOnlyNeededProps'

--- workoutLogWithRedux/src/components/Main.js
+++ workoutLogWithRedux/src/components/Main.js
@@ -3,9 +3,10 @@ import AddWorkoutForm from './AddWorkoutForm'
 import Workout from './Workout'
 export default class Main extends Component {
   render () {
+    const {addWorkout} = {...this.props}
     return (
-      <div>
-        <AddWorkoutForm {...this.props}/>
+        <div>
+        <AddWorkoutForm addWorkout={addWorkout}/>
         {this.props.workouts.workouts.map((workout, i) => (
            <Workout key={i} index={i} workout={workout} {...this.props}/>
          ))}
